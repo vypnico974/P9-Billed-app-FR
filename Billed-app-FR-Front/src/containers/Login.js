020d317dc24e32bb95b16a5ffeda3da84259b6f4
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2022-10-19 09:44:04 +0400

    connexion  admin

--- Billed-app-FR-Front/src/containers/Login.js
+++ Billed-app-FR-Front/src/containers/Login.js
@@ -18,7 +18,7 @@ export default class Login {
   handleSubmitEmployee = e => {
     e.preventDefault()
     const user = {
-      type: "Employee",
+      type: "Admin",
       email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
       status: "connected"
@@ -41,8 +41,8 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
